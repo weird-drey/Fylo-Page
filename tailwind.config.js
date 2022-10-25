4ad6c6ed17fc0fe50685b90a00d84f9f86d28ca1
Drey <dareybee@gmail.com>
2022-10-25 19:45:38 -0400

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: "Raleway"
+      },
+    },
+  },
+  plugins: [],
+}
